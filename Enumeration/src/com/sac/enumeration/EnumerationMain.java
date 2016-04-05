193cb9381de252a2997eacb96062f59b9c7894f8
S R Naik <srnaik@hotmail.com>
2016-04-04 20:38:24 -0500

    ENumeration Example

---
+++ Enumeration/src/com/sac/enumeration/EnumerationMain.java
@@ -0,0 +1,25 @@
+package com.sac.enumeration;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class EnumerationMain {
+
+	public static void main(String[] args) {
+		
+		Enumeration<String> enumeration;
+		
+		Vector<String> namesOfDays = new Vector<>();
+		namesOfDays.addElement("Sunday");
+		namesOfDays.addElement("Monday");
+		System.out.println(namesOfDays.add("Tuesday"));
+		
+		enumeration = namesOfDays.elements();
+		
+		while(enumeration.hasMoreElements()){
+			System.out.println(enumeration.nextElement());
+		}
+
+	}
+
+}
